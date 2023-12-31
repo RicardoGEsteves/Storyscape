3bf7c25628b47d453a2d6560a7cdc536d78e747f
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-31 02:29:13 +0000

    feat: :technologist: Type: Enhancement | Title: Addition of new hooks for various functionalities
    Description:
    The proposed changes introduce a set of new hooks in the application. These hooks are designed to provide various functionalities such as changing user image, creating bucket URL, creating comments, likes, posts, deleting comments, likes, posts, getting all posts, comments by post ID, likes by post ID, post by ID, posts by user, checking if a post is liked, searching profiles by name, updating profile image and profile.
    
    Main Files Walkthrough:
    - hooks/use-change-user-image.ts: A new hook 'useChangeUserImage' is added. This hook is used to change the user's image. It fetches the image, crops it, resizes it, and then uploads it to the storage. If the current image is not the default image, it deletes the current image.
    - hooks/use-create-bucket-url.ts: A new hook 'useCreateBucketUrl' is added. This hook is used to create a bucket URL.
    - hooks/use-create-comment.ts: A new hook 'useCreateComment' is added. This hook is used to create a comment in the database.
    - hooks/use-create-like.ts: A new hook 'useCreateLike' is added. This hook is used to create a like in the database.
    - hooks/use-create-post.ts: A new hook 'useCreatePost' is added. This hook is used to create a post in the database and upload the associated file to the storage.
    - hooks/use-delete-comment.ts: A new hook 'useDeleteComment' is added. This hook is used to delete a comment from the database.
    - hooks/use-delete-like.ts: A new hook 'useDeleteLike' is added. This hook is used to delete a like from the database.
    - hooks/use-delete-post-by-id.ts: A new hook 'useDeletePostById' is added. This hook is used to delete a post by its ID. It deletes all likes and comments associated with the post, then deletes the post itself from the database and the associated file from the storage.
    - hooks/use-get-all-posts.ts: A new hook 'useGetAllPosts' is added. This hook is used to get all posts from the database. It also fetches the profile associated with each post.
    - hooks/use-get-comments-by-post-id.ts: A new hook 'useGetCommentsByPostId' is added. This hook is used to get all comments associated with a post by its ID. It also fetches the profile associated with each comment.
    - hooks/use-get-likes-by-post-id.ts: A new hook 'useGetLikesByPostId' is added. This hook is used to get all likes associated with a post by its ID.
    - hooks/use-get-post-by-id.ts: A new hook 'useGetPostById' is added. This hook is used to get a post by its ID. It also fetches the profile associated with the post.
    - hooks/use-get-posts-by-user.ts: A new hook 'useGetPostsByUser' is added. This hook is used to get all posts associated with a user by the user's ID.
    - hooks/use-is-liked.ts: A new hook 'useIsLiked' is added. This hook is used to check if a post is liked by a user.
    - hooks/use-search-profiles-by-name.ts: A new hook 'useSearchProfilesByName' is added. This hook is used to search profiles by name.
    - hooks/use-update-profile-image.ts: A new hook 'useUpdateProfileImage' is added. This hook is used to update the profile image in the database.
    - hooks/use-update-profile.ts: A new hook 'useUpdateProfile' is added. This hook is used to update the profile in the database.
    
    Custom hooks created to improve development and logic implementation.

---
+++ hooks/use-update-profile-image.ts
@@ -0,0 +1,18 @@
+import { database } from "@/libs/appwrite-client";
+
+const useUpdateProfileImage = async (id: string, image: string) => {
+  try {
+    await database.updateDocument(
+      String(process.env.NEXT_PUBLIC_DATABASE_ID),
+      String(process.env.NEXT_PUBLIC_COLLECTION_ID_PROFILE),
+      id,
+      {
+        image: image,
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default useUpdateProfileImage;
