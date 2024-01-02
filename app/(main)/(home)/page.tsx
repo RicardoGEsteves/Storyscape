1f87575160184b3ef42c29a2202213ad14b12af2
Ricardo Esteves <ricardoglsesteves@gmail.com>
2024-01-02 09:55:47 +0000

    refactor: :children_crossing: Type: Improvements ,Refactoring | Title: Refactoring HomePage Component to Use Post Store
    Description:
    The HomePage component has been refactored to use the post store instead of hard-coded data. The useEffect hook is used to set all posts when the component mounts. The posts are then mapped to the PostMain component for display.
    
    Main Files Walkthrough:
    - app/(main)/(home)/page.tsx: The HomePage component now imports the usePostStore hook from the post store. A useEffect hook has been added to call the setAllPosts function from the post store when the component mounts. The hard-coded post data has been replaced with a map function that renders a PostMain component for each post in the allPosts array from the post store.
    
    Improved home page to receive dynamic data and render it.

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -1,24 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+
 import ClientOnly from "@/components/client-only";
+import { usePostStore } from "@/store/post";
 import PostMain from "../_components/post-main";
 
 export default function HomePage() {
+  const { allPosts, setAllPosts } = usePostStore();
+
+  useEffect(() => {
+    setAllPosts();
+  }, [setAllPosts]);
+
   return (
     <main className="mt-[80px] w-[calc(100%-90px)] max-w-[690px] ml-auto">
       <ClientOnly>
-        <PostMain
-          post={{
-            id: "1",
-            user_id: "1",
-            video_url: "/mockVideo.mp4",
-            text: "text",
-            created_at: "date",
-            profile: {
-              user_id: "1",
-              name: "user name",
-              image: "https://placehold.co/100",
-            },
-          }}
-        />
+        {allPosts.map((post) => (
+          <PostMain
+            post={post}
+            key={post.id}
+          />
+        ))}
       </ClientOnly>
     </main>
   );
